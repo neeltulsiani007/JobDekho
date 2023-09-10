afe9836a75de3b8c12ae0a9030c03675947e0604
neel <neeltulsiani007@gmail.com>
2023-09-10 06:49:13 +0530

    api url changed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:4000';
+//const BASE_URL = 'http://localhost:4000';
+const BASE_URL = `https://jobdekho.azurewebsites.net`;
 
 export default axios.create({
     baseURL: BASE_URL
