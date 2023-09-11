1569d2558935fd8eab0cba7a3c2e28f28878de3d
neel <neeltulsiani007@gmail.com>
2023-09-12 03:09:24 +0530

    app changed

--- client/src/homebaseUrl.js
+++ client/src/homebaseUrl.js
@@ -1,4 +1,4 @@
- const homebaseUrl = "https://jobdekhoo.azurewebsites.net";
+ const homebaseUrl = "https://jdekho.azurewebsites.net";
  //const homebaseUrl = "http://localhost:3000";
 
  export default homebaseUrl;
